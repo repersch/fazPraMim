61d0abfbf37aafbb5a6c3500af456c86d32c5038
Renata Persch <renatapersch@hotmail.com>
2023-02-13 19:25:41 -0300

    Primeiro commit

---
+++ backend/fazpramim/src/main/kotlin/br/edu/ifsp/scl/fazpramim/FazpramimApplication.kt
@@ -0,0 +1,11 @@
+package br.edu.ifsp.scl.fazpramim
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class FazpramimApplication
+
+fun main(args: Array<String>) {
+	runApplication<FazpramimApplication>(*args)
+}
---
+++ backend/fazpramim/src/test/kotlin/br/edu/ifsp/scl/fazpramim/FazpramimApplicationTests.kt
@@ -0,0 +1,13 @@
+package br.edu.ifsp.scl.fazpramim
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class FazpramimApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
