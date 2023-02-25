29e5630149bd3520c3b94689b689433a1f1b8784
Jones <jmmarao@gmail.com>
2023-02-24 22:19:38 -0300

    Adicionado componente do cabeçalho estilizado

--- frontend/faz-para-mim-app/src/app/app.module.ts
+++ frontend/faz-para-mim-app/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { Footer } from './components/Footer/footer.component';
+import { Header } from './components/Header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    Footer
+    Footer,
+    Header
   ],
   imports: [
     BrowserModule
---
+++ frontend/faz-para-mim-app/src/app/components/Header/header.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'headerPage',
+    templateUrl: './header.component.html',
+    styleUrls: ['./styles.css']
+})
+
+export class Header { }
