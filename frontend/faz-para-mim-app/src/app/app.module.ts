560dd5f030325edf650c6e8a5ac7092f6325136d
Jones <jmmarao@gmail.com>
2023-02-24 21:56:52 -0300

    Organização do app component. Introduzida lógica do footer component

--- frontend/faz-para-mim-app/src/app/app.module.ts
+++ frontend/faz-para-mim-app/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { Footer } from './components/Footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Footer
   ],
   imports: [
     BrowserModule
---
+++ frontend/faz-para-mim-app/src/app/components/Footer/footer.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'footerPage',
+    templateUrl: './footer.component.html'
+})
+
+export class Footer { }
