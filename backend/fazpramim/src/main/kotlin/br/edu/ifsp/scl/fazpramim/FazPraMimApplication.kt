6f6b48415525fec1a85c28bb9e784160621496c5
Jones <jmmarao@gmail.com>
2023-03-01 22:29:01 -0300

    Adicionado configuração CORS para comunicação com front-end

--- backend/fazpramim/src/main/kotlin/br/edu/ifsp/scl/fazpramim/FazPraMimApplication.kt
+++ backend/fazpramim/src/main/kotlin/br/edu/ifsp/scl/fazpramim/FazPraMimApplication.kt
@@ -2,23 +2,44 @@ package br.edu.ifsp.scl.fazpramim
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.FilterRegistrationBean
+import org.springframework.context.annotation.Bean
+import org.springframework.core.Ordered
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder
-import java.util.*
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource
+import org.springframework.web.filter.CorsFilter
 
 @SpringBootApplication
-class FazPraMimApplication
+class FazPraMimApplication {
+    @Bean
+    fun simpleCorsFilter(): FilterRegistrationBean<CorsFilter> {
+        val source = UrlBasedCorsConfigurationSource()
+        val config = CorsConfiguration()
+        config.allowCredentials = true
+        config.allowedOrigins = listOf("http://localhost:4200")
+        config.allowedMethods = listOf("*")
+        config.allowedHeaders = listOf("*")
+        source.registerCorsConfiguration("/**", config)
+        val bean = FilterRegistrationBean(CorsFilter(source))
+        bean.order = Ordered.HIGHEST_PRECEDENCE
+        return bean
+    }
+}
 
 fun main(args: Array<String>) {
-	runApplication<FazPraMimApplication>(*args)
+    runApplication<FazPraMimApplication>(*args)
 
-	val encoders: MutableMap<String, PasswordEncoder> = HashMap()
-	val pbkdf2Encoder = Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256)
-	encoders["pbkdf2"] = pbkdf2Encoder
-	val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
-	passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder)
+    val encoders: MutableMap<String, PasswordEncoder> = HashMap()
+    val pbkdf2Encoder =
+        Pbkdf2PasswordEncoder("", 8, 185000, Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm.PBKDF2WithHmacSHA256)
+    encoders["pbkdf2"] = pbkdf2Encoder
+    val passwordEncoder = DelegatingPasswordEncoder("pbkdf2", encoders)
+    passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder)
 
-	val result = passwordEncoder.encode("admin")
-	println("Minha senha: $result")
+    val result = passwordEncoder.encode("admin")
+    println("Minha senha: $result")
+
 }
