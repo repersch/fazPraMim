93fc578af1a4660e248fb10e03bf55e8e8b43b2f
Jones <jmmarao@gmail.com>
2023-03-26 17:04:43 -0300

    Adicionando template mockado para serviços

--- frontend/faz-para-mim-app/src/app/components/Service/service.component.ts
+++ frontend/faz-para-mim-app/src/app/components/Service/service.component.ts
@@ -2,7 +2,8 @@ import { Component } from "@angular/core";
 
 @Component({
     selector: 'serviceTag',
-    templateUrl: './service.component.html'
+    templateUrl: './service.component.html',
+    styleUrls: ['./styles.css']
 })
 
 export class ServiceComponentPage { }
